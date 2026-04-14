0a04458d379cad062065d6e98491c5f8d8017a56
Pratyush112 <prasaha003@gmail.com>
2026-04-15 00:36:19 +0530

    grammar error and blog error fix

--- components/blogs/blogs-grid.tsx
+++ components/blogs/blogs-grid.tsx
@@ -158,7 +158,7 @@ export function BlogsGrid() {
         </motion.div>
 
         {isLoading && <p className="text-pumice mb-8">Loading blogs...</p>}
-        {error && <p className="text-pumice mb-8">{error}</p>}
+        {/* {error && <p className="text-pumice mb-8">{error}</p>} */}
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {filteredBlogs.map((blog, index) => (
